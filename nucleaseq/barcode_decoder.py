7a7edcaa9475ecfb1da8f355032f7c915c699d99
John Hawkins <hawkjo@gmail.com>
2017-06-13 23:00:53 -0500

    Initial commit.

---
+++ nucleaseq/barcode_decoder.py
@@ -0,0 +1,108 @@
+import numpy as np
+import SeqlevSphere
+import seqtools
+import cPickle
+import time
+import psutil
+
+
+class SeqlevBarcodeDecoder(object):
+    """
+    A class for decoding seqlev barcodes after possible introduction of errors.
+    """
+    # This decoder is a simple codebook lookup. For short enough barcodes (as determined by
+    # available memory), this is easily done.
+    #
+    # The basic strategy is to encode each possible barcode as an integer. We then use that integer
+    # as an index in our codebook array. At the indexed location is a reference to the barcode of
+    # interest. Specifically, each location contains an integer which is then the (index plus one)
+    # of the decoded valid codeword position in the codework list. The reason for (index plus one)
+    # is that we reserve zero to indicate undecodable words.
+
+    def __init__(self):
+        pass
+
+    def build_codebook_from_cw_fpath(self, cw_fpath, max_err):
+        """
+        Builds codebook given path to file with list of one barcode per line.
+
+            :str cw_fpath: barcode file path
+        """
+        self.max_err = max_err
+        self._codewords = [line.strip() for line in open(cw_fpath)]
+        self._set_cw_len()
+
+        if len(self._codewords) < 2**8:
+            dtype = np.uint8
+            cw_bytes = 1
+        elif len(self._codewords) < 2**16:
+            dtype = np.uint16
+            cw_bytes = 2
+        elif len(self._codewords) < 2**32:
+            dtype = np.uint32
+            cw_bytes = 4
+        elif len(self._codewords) < 2**64:
+            dtype = np.uint64
+            cw_bytes = 8
+        else:
+            raise ValueError('More than 2^64 barcodes currently not supported')
+
+        space_size = 4**self.cw_len + 1
+        needed_bytes = space_size * cw_bytes
+        free_bytes = psutil.virtual_memory().free
+        if needed_bytes > free_bytes:
+            raise RuntimeError('Not enough memory. {:,d} bytes needed, {:,d} bytes free'.format(
+                needed_bytes,
+                free_bytes
+            ))
+                
+        self._codebook = np.zeros((space_size,), dtype=dtype)
+
+        for i, cw in enumerate(self._codewords):
+            cw_idx = i + 1
+            for seq in SeqlevSphere.SeqlevSphere(cw, self.max_err):
+                seq_idx = seqtools.dna2num(seq)
+                self._codebook[seq_idx] = cw_idx
+
+
+    def _set_cw_len(self):
+        assert len(set(map(len, self._codewords))) == 1, \
+                'Barcodes not all constant length in {}'.format(cw_fpath)
+        self.cw_len = len(self._codewords[0])
+
+
+    def save_codebook(self, out_fpath):
+        with open(out_fpath, 'w') as out:
+            cPickle.dump((self._codewords, self.max_err, self._codebook), out)
+
+
+    def load_codebook(self, codebook_fpath):
+        with open(codebook_fpath) as f:
+            self._codewords, self.max_err, self._codebook = cPickle.load(f)
+        self._set_cw_len()
+
+
+    def decode(self, seq):
+        seq_idx = seqtools.dna2num(seq)
+        cw_idx = self._codebook[seq_idx]
+        if cw_idx == 0:
+            return
+        else:
+            cw_idx -= 1
+            return self._codewords[cw_idx]
+
+
+    def time_decoder(self, n_decodes=1000):
+        ground_truth = np.random.choice(self._codewords, n_decodes)
+        bcs = [seqtools.add_random_seqlev_errors(cw, self.max_err) for cw in ground_truth]
+
+        start_time = time.time()
+        decoded = map(self.decode, bcs)
+        end_time = time.time()
+        decode_time = end_time - start_time
+
+        for i, (gt, bc, dw) in enumerate(zip(ground_truth, bcs, decoded)):
+            if not gt == dw:
+                raise RuntimeError('Decoding Errors in Test: {} -> {} -> {}'.format(gt, bc, dw))
+
+        print 'Decoding time: {:.2f} seconds'.format(decode_time)

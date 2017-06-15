c40ce9990c293b63e27742a8b52a416ae5114184
John Hawkins <hawkjo@gmail.com>
2017-06-15 15:01:40 -0500

    SeqLev is not a metric! Generation now corrected

--- nucleaseq/barcode_generator.py
+++ nucleaseq/barcode_generator.py
@@ -1,8 +1,10 @@
+import sys
 import os
 import numpy as np
 import SeqlevSphere
 import seqlev_dist
 from seqtools import bases, dna_rev_comp, dna2num, num2dna
+import psutil
 
 
 def possible_barcode_iterator(k, AT_max, GC_max):
@@ -66,27 +68,68 @@ def idx_possible_barcode_iterator(k, AT_max, GC_max):
 
 
 class SeqlevBarcodeGenerator(object):
-    def __init__(self, bc_len, min_dist, seq_idx_iter_func=None):
+    """
+    A class to generate a set of barcodes via the Seqlev semimetric.
+    """
+    # Algorithm explanation:
+    #   
+    # In a metric space, one can reserve spheres around each new codeword of radius 2*max_err, and
+    # any remaining word not reserved by any codeword is a valid new codeword. The Seqlev
+    # semimetric does not give that guarantee.  However, the above algorithm is still useful in
+    # that any word so reseved is guaranteed to not be a valid codeword. Hence, we perform the
+    # following steps:
+    #
+    #   1. Alphabetically iterate (for the Conway closure) through the words of acceptable
+    #       composition until finding a word still marked with int(0). For that word, check its
+    #       decode sphere. If all words in the decode sphere are marked < int(2), add codeword.
+    #   2. After adding a codeword, reseve two surrounding spheres of different radii:
+    #       -A sphere of radius max_err, the decode sphere, marked with int(2)
+    #       -A sphere of radius 2*max_err, the approx encode sphere, marked with int(1), careful to
+    #       not overwrite any 2's
+    #   3. Repeat until space exhausted.
+
+    def __init__(self, bc_len, max_err, seq_idx_iter_func=None):
         self.bc_len = bc_len
-        self.min_dist = min_dist
+        self.max_err = max_err
         self._codewords = set()
         self.barcodes = set()
         self.manual_codewords = set()
-        self.reserved_words = np.zeros((4**self.bc_len, ), dtype=np.uint8)
+        needed_bytes = 4**self.bc_len
+        free_bytes = psutil.virtual_memory().free
+        if needed_bytes > free_bytes:
+            raise RuntimeError('Not enough memory. {:,d} bytes needed, {:,d} bytes free'.format(
+                needed_bytes,
+                free_bytes
+            ))
+
+        self.reserved_words = np.zeros((needed_bytes, ), dtype=np.uint8)
         if seq_idx_iter_func is not None:
             self.seq_idx_iter_func = seq_idx_iter_func
         else:
             self.seq_idx_iter_func = lambda : xrange(4**self.bc_len)
 
-    def _add_codeword(self, idx):
-        assert isinstance(idx, int), '{} is not a valid codeword. Must be int'.format(idx)
-        self._codewords.add(idx)
-        word = num2dna(idx, self.bc_len)
-        for seq_idx in SeqlevSphere.SeqlevSphere(word, self.min_dist-1).parallel_num_iterator():
-            self.reserved_words[seq_idx] = 1
+    def _add_codeword(self, cw_idx):
+        assert isinstance(cw_idx, int), '{} is not a valid codeword. Must be int'.format(cw_idx)
+        self._codewords.add(cw_idx)
+        for seq_idx in self.iterate_approx_encode_sphere(cw_idx):
+            if self.reserved_words[seq_idx] == 0:
+                # Important to not overwrite previous 2's
+                self.reserved_words[seq_idx] = 1
+        for seq_idx in self.iterate_decode_sphere(cw_idx):
+            self.reserved_words[seq_idx] = 2
+
+    def iterate_decode_sphere(self, center_idx):
+        word = num2dna(center_idx, self.bc_len)
+        for seq_idx in SeqlevSphere.SeqlevSphere(word, self.max_err).parallel_num_iterator():
+            yield seq_idx
+
+    def iterate_approx_encode_sphere(self, center_idx):
+        word = num2dna(center_idx, self.bc_len)
+        for seq_idx in SeqlevSphere.SeqlevSphere(word, 2*self.max_err).parallel_num_iterator():
+            yield seq_idx
 
     def _add_barcode(self, seq_idx):
-        assert not self._idx_is_reserved(seq_idx), seq_idx
+        assert self._idx_is_available(seq_idx), seq_idx
         self.barcodes.add(seq_idx)
         self._codewords.add(seq_idx)
         self._add_codeword(seq_idx)
@@ -99,14 +142,32 @@ class SeqlevBarcodeGenerator(object):
         self.manual_codewords.add(seq_idx)
         self._add_codeword(seq_idx)
 
-    def _idx_is_reserved(self, idx):
-        return self.reserved_words[idx]
+    def dnastr_codeword_is_available(self, dnastring):
+        seq_idx = dna2num(dnastring)
+        self._idx_is_available(seq_idx)
+        
+    def _idx_is_available(self, test_idx):
+        if self.reserved_words[test_idx] != 0:
+            return False
+        else:
+            for seq_idx in self.iterate_decode_sphere(test_idx):
+                if self.reserved_words[seq_idx] == 2:
+                    return False
+            return True
 
     def Conway_closure(self):
         for seq_idx in self.seq_idx_iter_func():
-            if not self._idx_is_reserved(seq_idx):
+            if self._idx_is_available(seq_idx):
                 self._add_barcode(seq_idx)
                 print len(self.barcodes),
+
+    def Conway_closure_until_satisfied(self, n_desired_barcodes):
+        for seq_idx in self.seq_idx_iter_func():
+            if self._idx_is_available(seq_idx):
+                self._add_barcode(seq_idx)
+                print len(self.barcodes), num2dna(seq_idx, self.bc_len)
+                if len(self.barcodes) >= n_desired_barcodes:
+                    return
 
     @property
     def dna_barcodes(self):
@@ -119,28 +180,39 @@ class SeqlevBarcodeGenerator(object):
             for j in range(i+1, len(self.barcodes)):
                 bc2 = num2dna(bc_list[j], self.bc_len)
                 dist = seqlev_dist.seqlev_dist(bc1, bc2)
-                if dist < self.min_dist:
+                if dist < self.max_err:
                     print '!'*10 + ' FAIL ' + '!'*10
                     print 'Distance {} between {} and {}.'.format(dist, bc1, bc2)
                     return
         print 'Barcodes Pass Manual Check'
 
-def write_barcodes(bc_len, min_dist, dpath):
+
+def write_barcodes(bc_len, max_err, dpath):
     import time
     start_time = time.time()
-    fpath = os.path.join(dpath, 'barcodes{}-{}.txt'.format(bc_len, min_dist))
+    fpath = os.path.join(dpath, 'barcodes{}-{}.txt'.format(bc_len, max_err))
     GC_max = min(range(bc_len), key=lambda x: abs(float(x)/bc_len-0.6))
+    print 'Barcode length:', bc_len
     print 'AT/GC max:', GC_max
     bc_iter = idx_possible_barcode_iterator(bc_len, GC_max, GC_max)
-    sbg = SeqlevBarcodeGenerator(bc_len, min_dist, bc_iter)
+    sbg = SeqlevBarcodeGenerator(bc_len, max_err, bc_iter)
     sbg.Conway_closure()
     with open(fpath, 'w') as out:
         out.write('\n'.join(sorted(sbg.dna_barcodes)))
     comp_time = time.time() - start_time
     print
     print 'Barcode generation time:', comp_time
-    stats_fpath = os.path.join(dpath, 'barcodes{}-{}_stats.txt'.format(bc_len, min_dist))
+    stats_fpath = os.path.join(dpath, 'barcodes{}-{}_stats.txt'.format(bc_len, max_err))
     with open(stats_fpath, 'w') as out:
         out.write('Barcode length:\t{:d}\n'.format(bc_len))
-        out.write('Min-dist:\t{:d}\n'.format(min_dist))
+        out.write('Max-error:\t{:d}\n'.format(max_err))
         out.write('Barcode generation time:\t{:.2f} seconds\n'.format(comp_time))
+
+
+if __name__ == '__main__':
+    usg = '{} <bc_len> <max_err> <out_dir>'.format(sys.argv[0])
+    if len(sys.argv) != len(usg.split()):
+        sys.exit('Usage: {}'.format(usg))
+    bc_len, max_err = map(int, sys.argv[1:3])
+    dpath = sys.argv[3]
+    write_barcodes(bc_len, max_err, dpath)
